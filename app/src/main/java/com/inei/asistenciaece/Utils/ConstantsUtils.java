75ae5bf7c497e8b16fe3059c257aa966af0c488e
Ronald Velasquez <ronaldvg93@gmail.com>
2015-09-24 10:37:19 -0500

    constantes util

--- app/src/main/java/com/inei/asistenciaece/Utils/ConstantsUtils.java
+++ app/src/main/java/com/inei/asistenciaece/Utils/ConstantsUtils.java
@@ -3,13 +3,13 @@ package com.inei.asistenciaece.Utils;
 import java.util.HashMap;
 
 public class ConstantsUtils {
-    public static final String URL_LOGIN = "http://172.16.100.61/ece_capacitacion/tablet-login";
-    public static final String URL_VERSION = "http://172.16.100.61/ece_capacitacion/tablet-version";
-    public static final String URL_CONSOLIDATED = "http://172.16.100.61/ece_capacitacion/dashboard";
-    public static final String URL_PADRON= "http://172.16.100.61/ece_capacitacion/tablet-padron";
-    public static final String URL_SYNC_PADRON= "http://172.16.100.61/ece_capacitacion/tablet-sincronizar-padron";
+    public static final String HOSTNAME     = "http://capacitacion.amautas.net";
+    public static final String URL_LOGIN    = HOSTNAME + "/tablet-login";
+    public static final String URL_VERSION  = HOSTNAME + "/tablet-version";
+    public static final String URL_CONSOLIDATED = HOSTNAME + "/dashboard";
+    public static final String URL_PADRON   = HOSTNAME + "/tablet-padron";
+    public static final String URL_SYNC_PADRON  = HOSTNAME + "/tablet-sincronizar-padron";
     public static final HashMap<String, String> parameterError = new HashMap<String, String>(){{put("error", "No hay nada");}};
-
     /*public static final String URL_PADRON= "http://192.95.3.149:90/ece_ws/tablet-padron";*/
     /*public static final String URL_LOGIN = "http://192.95.3.149:90/ece_ws/tablet-login";*/
     /*public static final String URL_VERSION = "http://192.95.3.149:90/ece_ws/tablet-version";*/
